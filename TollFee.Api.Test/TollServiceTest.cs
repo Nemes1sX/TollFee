ee7dd030f7643bbb5a8f3829e78ecdf3f346443a
Nemes1sX <kazstonk@gmail.com>
2023-01-21 17:05:45 +0200

    Added tests for TollService (TDD)

---
+++ TollFee.Api.Test/TollServiceTest.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+using TollFee.Api.Services;
+
+namespace TollFee.Api.Test
+{
+    public class TollServiceTests
+    {
+        private TollService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+           _service = new TollService();
+        }
+
+        [Test]
+        public void TollService_CalculateToll_CalculaeSomeFee()
+        {
+            //Arrange
+            var request = new List<DateTime>()
+            {
+                new DateTime(2021, 12, 1, 7, 30, 1),
+                new DateTime(2021, 12, 1, 9, 30, 1),
+                new DateTime(2021, 1, 1),
+                new DateTime(2021, 1, 2)
+            }.ToArray();
+
+            //Act
+            var response = _service.CalculateFee(request);
+
+            //Assert
+            Assert.Equals(30, response.TotalFee);
+            Assert.Equals(7, response.AverageFeePerDay);
+        }
+    }
+}
