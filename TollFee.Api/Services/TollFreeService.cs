22f11b4e0c4f13dc784385f17c003f0fab9ee0d8
Nemes1sX <kazstonk@gmail.com>
2023-01-21 19:16:45 +0200

    Remove unused hardcoded working free days data

--- TollFee.Api/Services/TollFreeService.cs
+++ TollFee.Api/Services/TollFreeService.cs
@@ -26,23 +26,6 @@ namespace TollFee.Api.Services
             }
         }
 
-        /* internal static DateTime[] OtherFreeDays = new[]
-         {
-             new DateTime(2021, 1, 1),
-             new DateTime(2021, 1, 5),
-             new DateTime(2021, 1, 6),
-             new DateTime(2021, 4, 1),
-             new DateTime(2021, 4, 2),
-             new DateTime(2021, 4, 5),
-             new DateTime(2021, 4, 30),
-             new DateTime(2021, 5, 12),
-             new DateTime(2021, 5, 13),
-             new DateTime(2021, 6, 25),
-             new DateTime(2021, 11, 5),
-             new DateTime(2021, 12, 24),
-             new DateTime(2021, 12, 31)
-         };*/
-
        private  DateTime[] FreeDays()
         {
                 return _dbContext.TollFrees.Select(x => x.Date).ToArray();  
