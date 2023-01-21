0678de59f6b1e5f45d6c0e08e4b30421fbc69b41
Nemes1sX <kazstonk@gmail.com>
2023-01-21 20:07:35 +0200

    Added existing year validation rule to check if requested year exists in DB

---
+++ TollFee.Api/Models/ValidationRules/ExistingYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace TollFee.Api.Models.ValidationRules
+{
+    public class ExistingYear : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var dateTimeRequests = (DateTime[])validationContext.ObjectInstance;
+            var _db = (TollDBContext)validationContext.GetService(typeof(TollDBContext));
+            var nonExistingYear = false;
+
+            foreach(var dateTimeRequest in dateTimeRequests)
+            {
+                if (_db.TollFrees.Any(x => x.Year != dateTimeRequest.Year)) 
+                { 
+                    nonExistingYear = true;
+                    break;
+                }
+            }
+
+            return nonExistingYear 
+                ? new ValidationResult("Requested year doesn't exist in the DB")
+                : ValidationResult.Success;
+        }
+    }
+}
