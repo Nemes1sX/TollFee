e5841060f2d0fdf1977d5b05aaf3f5612aca42db
Nemes1sX <kazstonk@gmail.com>
2023-01-23 20:31:15 +0200

    Add endpoint for user to edit year

---
+++ TollFee.Api/Controllers/ConfigController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TollFee.Api.Config;
+
+namespace TollFee.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfigService _configService;
+
+        public ConfigController(IConfigService configService)
+        {
+            _configService = configService;
+        }
+
+        [HttpPost]
+        [Route("SetYear")]
+        public IActionResult SetYear([FromBody][Required, Range(2000, 2025)]int year)
+        {
+            _configService.SetYear(year);
+
+            return Ok(new {messge = string.Format("{0} is set", year)});
+        }
+    }
+}
